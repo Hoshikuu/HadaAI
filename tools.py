788cb92a95ef458735f257df38b684dcf7504ec1
Hoshiku <145494860+Hoshikuu@users.noreply.github.com>
2026-03-11 20:09:35 +0100

    Cambiado a modelo local, mejora de prompt, usando llama.cpp, compilado de source code

--- tools.py
+++
@@ -1,7 +0,0 @@
-def hada_system():
-    with open("prompts/hada.txt", "r", encoding="utf-8") as f:
-        return f.read()
-    
-def summary_system():
-    with open("prompts/summary.txt", "r", encoding="utf-8") as f:
-        return f.read()
