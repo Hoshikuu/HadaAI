e2e995ec8c17f54bbe8b07e92b99ca8304e40462
Hoshiku <145494860+Hoshikuu@users.noreply.github.com>
2026-03-22 13:57:57 +0100

    Better documentation

--- hada/mem_init.py
+++ hada/mem_init.py
@@ -1,41 +1,69 @@
 #   ----------------------------------------------------
 #          Hoshikuu - https://github.com/Hoshikuu
 #   ----------------------------------------------------
-#   HadaAI/hada/mem_init.py - V0.1.1
+#   HadaAI/hada/mem_init.py - V0.1.2
 
 import datetime
 from os.path import isfile
 
 class Mem():
+    """Memory Class, manage the memory of Hada, for now only DAILY memory
+
+    Methods\n
+    |    -> Register()\n
+    |    -> ReadMem()\n
+    |    -> ReadFile()\n
+    |    -> CheckFile()\n
+    """
     def __init__(self):
-        self.mem_file = f"hada/mem/{datetime.date.today()}.hada"
-        self.content = ""
+        """Defines the mem_file where is the memory file for today
+        """
+        # Guarda las conversaciones enteras por dia
+        self.mem_file = f"hada/mem/{datetime.date.today()}.hada" # Fichero de memoria para cada dia
+        self.content = ""   # Contenido del fichero
 
     def CheckFile(self):
+        """Check if the memory file exists, if not creates it
+        """
+        #TODO Añadir para que cree la carpeta de mem
         if isfile(self.mem_file):
             return
         with open(self.mem_file, "w+", encoding="utf-8") as f:
             f.write("")
 
     def ReadFile(self):
+        """Reads the mem file of today
+        """
         self.CheckFile()
         with open(self.mem_file, "r", encoding="utf-8") as f:
             self.content = f.read()
 
-    def Register(self, _in, _out):
+    def Register(self, _in: str, _out: str):
+        """Call this function for register a new conversation of Hada
+
+        Args:
+            _in (str): User phrase
+            _out (str): Hada phrase
+        """
         self.mem_file = f"hada/mem/{datetime.date.today()}.hada"
         self.ReadFile()
 
+        # Escribe el contenido en el archivo
         with open(self.mem_file, "w+", encoding="utf-8") as f:
             self.content += f"_user_:{_in}\n"
             self.content += f"_hada_:{_out}\n"
             f.write(self.content)
 
     def ReadMem(self):
+        """Reads the memory file
+
+        Returns:
+            list: Return a list of processed chunks of the memory ready to iterates
+        """
         self.mem_file = f"hada/mem/{datetime.date.today()}.hada"
         self.ReadFile()
 
-        mem = []
+        mem = []    # La lista de memoria que se devuelve ya esta preparada para poder iterarse y ponerse dentro de Hada
         chunks = self.content.split("_user_:")
 
         for chunk in chunks:
--- main.py
+++ main.py
@@ -1,62 +1,97 @@
 #   ----------------------------------------------------
 #          Hoshikuu - https://github.com/Hoshikuu
 #   ----------------------------------------------------
-#   HadaAI/main.py - V0.1.0
+#   HadaAI/main.py - V0.1.1
 
 from asyncio import run, sleep, create_task, get_event_loop, CancelledError
 from openai import OpenAI
 from hada import Hada, Stt, Mem
 
-def HadaPrompt(version):
+def HadaPrompt(version: int):
+    """Reads the Hada System Prompt
+
+    Args:
+        version (int): The version of Hada System Prompt
+
+    Returns:
+        str: The content of that specific version
+    """
+    # Lee el archivo dependiendo de la version indicada
     with open(f"hada/prompts/hadaV{version}.txt", "r") as f:
         return f.read()
 
-def query_hada(prompt: str, mem: Mem):
-    """Bloqueante, corre en thread."""
+def QueryHada(prompt: str, mem: Mem):
+    """Query to ask to Hada
+
+    Args:
+        prompt (str): The prompt string to pass it to Hada
+        mem (Mem): The memory class initilized in main
+
+    Returns:
+        str: The response of Hada
+    """
+
+    # Url donde este hosteado Hada
     client = OpenAI(
         base_url="http://localhost:8000/v1",
         api_key="no-key"
     )
+
+    # Lee la memorua de Hada
     memories = mem.ReadMem()
+    # Añade el system prompt
     messages = [{"role": "system", "content": HadaPrompt(4)}]
+    # Añade el historial de mensajes de la memoria a mensajes
     for memory in memories:
         messages.append(memory)
+    # Añade el ultimo mensaje solicitado
     messages.append({"role": "user", "content": prompt})
+
+    # Hace la solicitud a Hada
     stream = client.chat.completions.create(
         model="Hoshiku/HadaAI",
         messages=messages,
         extra_body={
-            "chat_template_kwargs": {"enable_thinking": False},
+            "chat_template_kwargs": {"enable_thinking": False}, # Desactiva el Thinking de Hada
         }, 
-        stream=True,
+        stream=True, # Devuelve el texto en trozos
     )
+
+    # Procesamiento de la respuesta
     response = ""
     for chunk in stream:
         text = chunk.choices[0].delta.content or ""
+        # Hace el efecto de generacion de texto poco a poco
         print(text, end="", flush=True)
-        response += text
-    print()  # salto de línea al terminar
+        response += text # Guarda cada trozo en la variable de respuesta
+    print()
     return response
 
 async def main():
+    """Async main function
+    """
+    # Inicializa los modulos
     hada = Hada()
     stt = Stt()
     mem = Mem()
 
+    # No se que hace
     loop = get_event_loop()
+    # Crea las tareas para ejecutar Hada y Stt
     task_hada = create_task(hada.StartHada())
     task_stt = create_task(stt.StartStt())
 
     await sleep(10)
 
     try:
+        # Funcion principal para hacer preguntas a Hada
         while True:
             prompt = await loop.run_in_executor(None, stt.Play)
             await sleep(1)
             if prompt is None:
                 break
             print(prompt)
-            response = await loop.run_in_executor(None, query_hada, prompt, mem)
+            response = await loop.run_in_executor(None, QueryHada, prompt, mem)
             mem.Register(prompt, response)
     except (KeyboardInterrupt, CancelledError):
         pass
@@ -67,6 +102,8 @@ async def main():
         await task_hada
 
 if __name__ == "__main__":
+    """Main function
+    """
     try:
         run(main())
     except KeyboardInterrupt:
